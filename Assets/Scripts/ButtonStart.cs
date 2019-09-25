8866f6c1dc6dafb77271b4f6f90fa1b8921aeacd
sresl <sarah.resl21@gmail.com>
2019-09-25 13:22:23 +0200

    Unity Progress

---
+++ Assets/Scripts/ButtonStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStart : MonoBehaviour
+{
+    public void changescene (string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
---
+++ Assets/Scripts/RandomColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomColor : MonoBehaviour
+{
+  
+    public Color[] ColorBank;
+    public bool sprite = false;
+    public SpriteRenderer s;
+    public int num;
+
+    void Start()
+    {
+        ChangeColor();
+            s = GetComponent<SpriteRenderer>();
+            s.color = ColorBank[num];
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeColor();
+            s.color = ColorBank[num];
+             
+            
+        }
+    }
+
+    public void ChangeColor(){
+        num = Random.Range(0, ColorBank.Length -1);
+    }
+}
